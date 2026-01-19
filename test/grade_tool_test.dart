a2b3f4e196cb05523c81d42dd7fe24e2bf40772a
n.smith <ndsmith2021@gmail.com>
2026-01-18 23:30:42 -0500

    Adding tests

--- test/grade_tool_test.dart
+++ test/grade_tool_test.dart
@@ -2,5 +2,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:grade_tool/grade_tool.dart';
 
 void main() {
+  final gradeNumber = GradeTool();
 
+  test('95 is an A', () {
+    expect('A', gradeNumber.convert(90));
+  });
+
+  test('82 is a B', () {
+    expect('B', gradeNumber.convert(82));
+  });
+
+  test('60 is a D', () {
+    expect('D', gradeNumber.convert(60));
+  });
 }
